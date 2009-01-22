f5eb96d0befcb63e733e0e00b94e1c929959c072
dimagidan <dmyung@dimagi.com>
2009-01-22 23:26:32 +0000

    test

---
+++ rapidandroid/org.rapidandroid/src/org/rapidandroid/content/translation/ParsedDataWrapper.java
@@ -0,0 +1,14 @@
+package org.rapidandroid.content.translation;
+
+/**
+ * @author Daniel Myung dmyung@dimagi.com
+ * @created Jan 21, 2009
+ * 
+ *          Helper class to simplify the insertion and querying of parsed form
+ *          data from the content provider
+ */
+
+
+public class ParsedDataWrapper {
+
+}
